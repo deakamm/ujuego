317821e7ceb35e483b73f9d33fa4f887017b53f8
mamadou <deakamm2@gmail.com>
2023-02-23 13:45:29 +0100

    commit for footer

--- src/Vistas/Main.jsx
+++ src/Vistas/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar';
 import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
 const Main = () => {
   return (
     <div>
         <Navbar/>
         <Header/>
+        <Footer/>
     </div>
   )
 }
