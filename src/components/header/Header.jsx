cb254bf3d211b4cd5854d14fd40dc00cd159294f
LolaGM <canal030w@gmail.com>
2023-02-23 14:47:59 +0100

    banner size solved but text is left to repair

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
          <div className='container-header'>
        <div className='container-fresh'>
-         <img className=" img-fluid" src={"/images/Rectangle 3.png"}  alt=""></img>
+         <img className="img-fluid" src={"/images/Rectangle 3.png"}  alt="Mando y consola de videojuegos" width="100%" />
        </div>  
        <div className='container-text'>
-          <p className='slogan'>Jugar solo es </p>
+          <p className='slogan'>Jugar solo es</p>
           <p className='claim'>el principio</p>
        </div>
     </div>  
