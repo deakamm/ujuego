0d844e9720312eef1155e3715fd1f2c7705e6e97
LolaGM <canal030w@gmail.com>
2023-02-23 10:55:18 +0100

    first steps in footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from './Footer.css';
+import Footer from react-bootstrap
+
+function Footer() {
+  return (
+    <div className='footerUgame'>Footer</div>
+  )
+};
+
+export default Footer;
